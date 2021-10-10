ceb60aac12925970656fcdfe083a92a4dd614b3c
Hoang Quang Hung <79563371+HunineZ@users.noreply.github.com>
2021-10-10 21:11:31 +0700

    Function ACF

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from os.path import join
-from numpy import sign
 from scipy.io.wavfile import read
+import numpy as np
 import matplotlib.pyplot as plt
 import sounddevice as sd
 import time
@@ -10,23 +10,31 @@ PHONE_F1 = join(FILE_PATH, "phone_F1.wav")
 PHONE_M1 = join(FILE_PATH, "phone_M1.wav")
 STUDIO_F1 = join(FILE_PATH, "studio_F1.wav")
 STUDIO_M1 = join(FILE_PATH, "studio_M1.wav")
+FRAME_LENGTH = 0.02
 
+def ACF(frequency, signal, begin):
+    # Độ dài của frame (đơn vị mẫu)
+    frameLength = int(FRAME_LENGTH * frequency)
+    lagRange = frequency//450, frequency//70
+    xx = []
+    for lag in range(lagRange[0], lagRange[1]):
+        x = [np.float64(i) for i in signal[begin : (begin + frameLength)]]
+        xx.append(sum([x[m] * x[m + lag] for m in range(frameLength - lag)]))
+    return xx
+
+# Main
 # Đọc tín hiệu mẫu
-frequency, signal = read(PHONE_M1)
+frequency, signal = read(PHONE_F1)
 print("Frequency : ", frequency)
 print("Signal : ", signal)
-
+print(ACF(frequency, signal, 9200))
 # Đồ thị
 plt.plot(signal)
 plt.ylabel("Amplitude")
 plt.xlabel("Time (samples)")
-plt.title("Phone M1")
 plt.show()
-
-# Tính độ dài của file audio
-duration = len(signal) / frequency
 
 # Khôi phục tín hiệu
 sd.play(signal, frequency)
-time.sleep(duration)
+time.sleep(len(signal) / frequency)
 sd.stop()
